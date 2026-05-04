124acdc3563f89d1b49c0a658c32e28b50cce0cb
Will Doherty <wdoherty8@gmail.com>
2026-05-04 16:54:27 +0100

    updated neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,8 +49,93 @@ vim.keymap.set({ "n", "x", "o" }, "nj", function() require("flash").jump() end, 
 require("nvim-surround").setup({})
 
 
+-- Treesitter syntax highlighting
+require("nvim-treesitter").setup()
+
+require("nvim-treesitter").install({
+  "python",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "c",
+  "cpp",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "python",
+    "lua",
+    "vim",
+    "help",
+    "c",
+    "cpp",
+  },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+
+
+-- Treesitter text objects
+require("nvim-treesitter-textobjects").setup({
+  select = {
+    lookahead = true,
+    selection_modes = {
+      ["@function.outer"] = "V",
+      ["@class.outer"] = "V",
+      ["@loop.outer"] = "V",
+      ["@conditional.outer"] = "V",
+    },
+    include_surrounding_whitespace = false,
+  },
+})
+
+local ts_select = require("nvim-treesitter-textobjects.select")
+
+vim.keymap.set({ "x", "o" }, "af", function()
+  ts_select.select_textobject("@function.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "if", function()
+  ts_select.select_textobject("@function.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ac", function()
+  ts_select.select_textobject("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ic", function()
+  ts_select.select_textobject("@class.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ap", function()
+  ts_select.select_textobject("@parameter.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ip", function()
+  ts_select.select_textobject("@parameter.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "al", function()
+  ts_select.select_textobject("@loop.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "il", function()
+  ts_select.select_textobject("@loop.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ai", function()
+  ts_select.select_textobject("@conditional.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ii", function()
+  ts_select.select_textobject("@conditional.inner", "textobjects")
+end)
+
+
 -- Language server
-vim.opt.completeopt = { "menuone", "noinsert", "noselect", "popup" }
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
 
 vim.lsp.config("jedi", {
   cmd = { "jedi-language-server" },
@@ -84,7 +169,7 @@ vim.lsp.config("jedi", {
     client.server_capabilities.semanticTokensProvider = nil
 
     vim.lsp.completion.enable(true, client.id, bufnr, {
-      autotrigger = false,
+      autotrigger = true,
     })
 
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
@@ -95,129 +180,6 @@ vim.lsp.config("jedi", {
     end, { buffer = bufnr })
   end,
 })
-
-vim.keymap.set("i", "<Tab>", function()
-  return vim.fn.pumvisible() == 1 and vim.keycode("<C-n>") or vim.keycode("<Tab>")
-end, { expr = true, silent = true })
-
-vim.keymap.set("i", "<S-Tab>", function()
-  return vim.fn.pumvisible() == 1 and vim.keycode("<C-p>") or vim.keycode("<S-Tab>")
-end, { expr = true, silent = true })
-
-vim.keymap.set("i", "<CR>", function()
-  return vim.fn.pumvisible() == 1 and vim.keycode("<C-y>") or vim.keycode("<CR>")
-end, { expr = true, silent = true })
-
-vim.lsp.enable("jedi")
-
-
--- Programming config
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"c", "cpp"},
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.expandtab = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-  end,
-})
-
-
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-
--- Colours
-vim.opt.termguicolors = true
-require("catppuccin").setup({
-  flavour = "mocha", -- latte, frappe, macchiato, mocha
-})
-vim.cmd.colorscheme("catppuccin")
-
-
--- File searching
-require("telescope").setup({})
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files)
-vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-vim.keymap.set("n", "<leader>fb", builtin.buffers)
-vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-
-
--- Within-file navigation
-require("flash").setup({})
-vim.keymap.set({ "n", "x", "o" }, "nj", function() require("flash").jump() end, { desc = "Flash" })
-
-
--- Surround
-require("nvim-surround").setup({})
-
-
--- Language server
-vim.opt.completeopt = { "menuone", "noinsert", "noselect", "popup" }
-
-vim.lsp.config("jedi", {
-  cmd = { "jedi-language-server" },
-  filetypes = { "python" },
-  root_markers = {
-    "pyproject.toml",
-    "setup.py",
-    "setup.cfg",
-    "requirements.txt",
-    ".git",
-  },
-  init_options = {
-    diagnostics = {
-      enable = false,
-    },
-    hover = {
-      enable = false,
-    },
-    completion = {
-      disableSnippets = true,
-    },
-  },
-  on_attach = function(client, bufnr)
-    client.server_capabilities.hoverProvider = false
-    client.server_capabilities.signatureHelpProvider = false
-    client.server_capabilities.referencesProvider = false
-    client.server_capabilities.documentHighlightProvider = false
-    client.server_capabilities.documentSymbolProvider = false
-    client.server_capabilities.workspaceSymbolProvider = false
-    client.server_capabilities.codeActionProvider = false
-    client.server_capabilities.semanticTokensProvider = nil
-
-    vim.lsp.completion.enable(true, client.id, bufnr, {
-      autotrigger = false,
-    })
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr })
-
-    vim.keymap.set("i", "<C-Space>", function()
-      vim.lsp.completion.get()
-    end, { buffer = bufnr })
-  end,
-})
-
-vim.keymap.set("i", "<Tab>", function()
-  return vim.fn.pumvisible() == 1 and vim.keycode("<C-n>") or vim.keycode("<Tab>")
-end, { expr = true, silent = true })
-
-vim.keymap.set("i", "<S-Tab>", function()
-  return vim.fn.pumvisible() == 1 and vim.keycode("<C-p>") or vim.keycode("<S-Tab>")
-end, { expr = true, silent = true })
-
-vim.keymap.set("i", "<CR>", function()
-  return vim.fn.pumvisible() == 1 and vim.keycode("<C-y>") or vim.keycode("<CR>")
-end, { expr = true, silent = true })
 
 vim.lsp.enable("jedi")
 
